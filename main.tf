5b09e7e5fd30570568c4d84dff5540c16e13096c
Armando Berlanga <armando.berlanga@slalom.com>
2025-06-05 16:58:06 -0600

    Add VPC module configuration and update load balancer settings for blog application

--- main.tf
+++ main.tf
@@ -14,6 +14,25 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "blog-vpc-dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -41,21 +60,38 @@ module "blog_security_group" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+module "blog_load_balancer" {
+  source = "terraform-aws-modules/alb/aws"
 
-  name = "blog-vpc-dev"
-  cidr = "10.0.0.0/16"
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  # Security Group
+  security_groups = [module.blog_security_group.security_group_id]
 
-  # enable_nat_gateway = true
-  # enable_vpn_gateway = true
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  listeners = {
+    ex-http = {
+      port     = 80
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
 
   tags = {
-    Terraform = "true"
     Environment = "dev"
   }
 }
