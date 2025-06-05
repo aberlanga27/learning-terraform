2d8c2c62b3f1e6155c74a2e10e04fd3020222ef0
Armando Berlanga <armando.berlanga@slalom.com>
2025-06-05 16:43:08 -0600

    Refactor VPC and security group configuration to use module for blog instance

--- main.tf
+++ main.tf
@@ -14,15 +14,12 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_security_group.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -33,7 +30,7 @@ module "blog_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
 
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = module.blog_vpc.vpc_id
   name        = "blog_sg"
   description = "Security group for the blog application"
 
@@ -42,4 +39,23 @@ module "blog_security_group" {
 
   egress_rules  = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "blog-vpc-dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true
+  # enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
